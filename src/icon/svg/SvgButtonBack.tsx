898e4ff772b4acd55bb2f9b749ba0f5da3b8cb26
hdeg <76611318+exit1991@users.noreply.github.com>
2023-05-09 22:59:07 +0900

    SVGアイコンのサイズと色をCSSから指定できるように調整 #14

--- src/icon/svg/SvgButtonBack.tsx
+++ src/icon/svg/SvgButtonBack.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default () => {
   return (
-    <svg width={150} height={150} viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx={75} cy={75} r={75} fill="#388E3C" />
-      <circle cx="75.0004" cy="75.0004" r="66.9643" fill="white" />
-      <circle cx="74.9999" cy="74.9999" r="58.9286" fill="#388E3C" />
+    <svg viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg" className="p-svg-button-back">
+      <circle cx={75} cy={75} r={75} className="p-svg-button-back__color-group-base" />
+      <circle cx="75.0004" cy="75.0004" r="66.9643" className="p-svg-button-back__color-group-accent" />
+      <circle cx="74.9999" cy="74.9999" r="58.9286" className="p-svg-button-back__color-group-base" />
       <g clipPath="url(#clip0_156_121)">
-        <path d="M101.718 57.622C96.7393 52.6282 89.7648 49.5135 82.1528 49.5181H72.1774V47.9696V37.6445L46.5324 58.4977L72.1774 79.3529V68.7122V67.5874H82.1528C84.8316 67.5919 87.1753 68.6483 88.9411 70.3974C90.6914 72.1643 91.7478 74.5068 91.7522 77.1867C91.7478 79.8666 90.6913 82.2092 88.9411 83.9738C87.1753 85.7252 84.8316 86.7815 82.1528 86.7861H40.1787V104.855H82.1528C89.7648 104.86 96.7393 101.745 101.718 96.7516C106.709 91.7732 109.826 84.7987 109.822 77.1867C109.826 69.5748 106.709 62.6002 101.718 57.622Z" fill="white" />
+        <path d="M101.718 57.622C96.7393 52.6282 89.7648 49.5135 82.1528 49.5181H72.1774V47.9696V37.6445L46.5324 58.4977L72.1774 79.3529V68.7122V67.5874H82.1528C84.8316 67.5919 87.1753 68.6483 88.9411 70.3974C90.6914 72.1643 91.7478 74.5068 91.7522 77.1867C91.7478 79.8666 90.6913 82.2092 88.9411 83.9738C87.1753 85.7252 84.8316 86.7815 82.1528 86.7861H40.1787V104.855H82.1528C89.7648 104.86 96.7393 101.745 101.718 96.7516C106.709 91.7732 109.826 84.7987 109.822 77.1867C109.826 69.5748 106.709 62.6002 101.718 57.622Z" className="p-svg-button-back__color-group-accent" />
       </g>
       <defs>
         <clipPath id="clip0_156_121">
-          <rect width="69.6429" height="69.6429" fill="white" transform="translate(40.1787 36.4287)" />
+          <rect width="69.6429" height="69.6429" transform="translate(40.1787 36.4287)" className="p-svg-button-back__color-group-accent" />
         </clipPath>
       </defs>
     </svg>
--- src/icon/svg/SvgButtonPrev.tsx
+++ src/icon/svg/SvgButtonPrev.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default () => {
   return (
-    <svg width={200} height={200} viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx={100} cy={100} r={100} fill="#2979FF" />
-      <circle cx="100.001" cy="99.9996" r="89.2857" fill="white" />
-      <circle cx={100} cy={100} r="78.5714" fill="#2979FF" />
+    <svg viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg" className="p-svg-button-prev">
+      <circle cx={100} cy={100} r={100} className="p-svg-button-prev__color-group-base" />
+      <circle cx="100.001" cy="99.9996" r="89.2857" className="p-svg-button-prev__color-group-accent" />
+      <circle cx={100} cy={100} r="78.5714" className="p-svg-button-prev__color-group-base" />
       <g clipPath="url(#clip0_143_47)">
-        <path d="M97.7814 142.062C101.931 146.011 108.497 145.849 112.446 141.699C116.398 137.545 116.236 130.978 112.082 127.03L94.5785 110.374H136.054C141.785 110.374 146.429 105.732 146.429 99.9992C146.429 94.2689 141.785 89.6268 136.054 89.6268H94.5787L112.082 72.9687C116.236 69.023 116.398 62.4536 112.447 58.3035C108.497 54.1534 101.932 53.9874 97.7815 57.9388L53.5716 99.9992L97.7814 142.062Z" fill="white" />
+        <path d="M97.7814 142.062C101.931 146.011 108.497 145.849 112.446 141.699C116.398 137.545 116.236 130.978 112.082 127.03L94.5785 110.374H136.054C141.785 110.374 146.429 105.732 146.429 99.9992C146.429 94.2689 141.785 89.6268 136.054 89.6268H94.5787L112.082 72.9687C116.236 69.023 116.398 62.4536 112.447 58.3035C108.497 54.1534 101.932 53.9874 97.7815 57.9388L53.5716 99.9992L97.7814 142.062Z" className="p-svg-button-prev__color-group-accent" />
       </g>
       <defs>
         <clipPath id="clip0_143_47">
-          <rect width="92.8571" height="92.8571" fill="white" transform="matrix(-1 0 0 -1 146.429 146.429)" />
+          <rect width="92.8571" height="92.8571" transform="matrix(-1 0 0 -1 146.429 146.429)" className="p-svg-button-prev__color-group-accent" />
         </clipPath>
       </defs>
     </svg>
